28074c7a14f8302b97d8a397ed7e13d42c56a9ab
eyikilmaz <yeyikilmaz@gmail.com>
2022-11-24 00:22:27 +0300

    features: added Api.Application added IGenericRepository

---
+++ src/Api/Core/CustomBlog.Api.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,34 @@
+﻿using CustomBlog.Api.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomBlog.Api.Application.Interfaces.Repositories;
+
+public interface IGenericRepository<TEntity> where TEntity: BaseEntity
+{
+    Task<int> AddAsync(TEntity entity);
+    int Add(TEntity entity);
+    Task<int> AddAsync(IEnumerable<TEntity> entities);
+    int Add(IEnumerable<TEntity> entities);
+
+    Task<int> UpdateAsync(TEntity entity);
+    int Update(TEntity entity);
+
+    Task<int> DeleteAsync(TEntity entity);
+    int Delete(TEntity entity);
+    Task<int> DeleteAsync(Guid id);
+    int Delete(Guid id);
+    Task<bool> DeleteRangeAsync(Expression<Func<TEntity, bool>> predicate);
+    bool DeleteRange(Expression<Func<TEntity, bool>> predicate);
+
+    Task<int> AddOrUpdateAsync(TEntity entity);
+    int AddOrUpdate(TEntity entity);
+    IQueryable<TEntity> AsQueryable();
+    Task<List<TEntity>> GelAll(bool noTracking = true);
+
+
+}
